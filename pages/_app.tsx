80647691317e192eada120ad44b68bdd3eff7e47
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2026-02-06 14:42:23 -0800

    feat(03-04): integrate PageTransition into app layout
    - Wrap page Component with PageTransition in _app.tsx
    - All route navigations now have slide+fade animation
    - Uses Next.js router pathname as animation key

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,9 +6,14 @@ import '@fontsource/noto-sans-myanmar/500.css';
 import '@fontsource/noto-sans-myanmar/700.css';
 
 import '../src/styles/globals.css';
+import { PageTransition } from '@/components/animations/PageTransition';
 
 const CivicPrepApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <PageTransition>
+      <Component {...pageProps} />
+    </PageTransition>
+  );
 };
 
 export default CivicPrepApp;
